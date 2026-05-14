205bd395edaa9170e5c5ac7a4affc49ef3f00aa4
b1yd <2156864690@qq.com>
2026-05-14 18:24:32 +0800

    test files

---
+++ tests/ui/parser/field-name-in-tuple-struct.rs
@@ -0,0 +1,22 @@
+// Provide diagnostics when the user writes field names in tuple struct.
+
+struct Foo(a:u8,b:u8);
+//~^ ERROR field names are not allowed in tuple structs
+//~| HELP if you wanted to create a tuple struct, remove field names
+//~| HELP if you wanted to create a regular struct, use curly braces
+
+struct Bar(a:u8,u8,c:u8);
+//~^ ERROR field names are not allowed in tuple structs
+//~| HELP if you wanted to create a tuple struct, remove field names
+//~| HELP if you wanted to create a regular struct, use curly braces
+
+enum Quz{
+    Foo(a:u8),
+//~^ ERROR field names are not allowed in tuple structs
+//~| HELP if you wanted to create a tuple struct, remove field names
+//~| HELP if you wanted to create a regular struct, use curly braces
+}
+//field name diagnostics should not activate in this case.
+// std::string is not a valid name.
+struct Baz(std::string:String);
+//~^ ERROR expected one of `!`, `(`, `)`, `+`, `,`, `::`, or `<`, found `:`
